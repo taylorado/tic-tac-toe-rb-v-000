ae3c0e38c3b9ba41edb9267ae033829805b2b023
Taylor Donlan <taylordonlan@gmail.com>
2017-11-17 21:44:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,7 @@ def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
      true
   else
-    puts "Invalid move.  Try again."
+  #  puts "Invalid move.  Try again."
      false
   end
 end
